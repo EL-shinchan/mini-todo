921c2f652f86e65aa828d19a9ddbcf1d6f264df4
Eddie <eddieliu0206@homtail.com>
2026-03-23 17:41:18 +0800

    build quote page

---
+++ quote-page/script.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+ const quoteText = document.getElementById("quoteText");
+   
+ const quoteButton = document.getElementById("quoteButton");
+
+ quoteButton.onclick = function () {
+ 
+ quoteText.textContent = "No pain, no gain.";
+ };
+};
