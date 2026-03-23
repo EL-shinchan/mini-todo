d8a2ef4352401cde92a6216a3f90b3af9379c1a9
Eddie <eddieliu0206@homtail.com>
2026-03-23 17:02:14 +0800

    build personal page

---
+++ personal-page/script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+ const button = document.getElementById("helloButton");
+
+  
+ button.onclick = function () {
+ alert("Yo Eddie, you just made your first interactive page.");
+     
+ };
+};
