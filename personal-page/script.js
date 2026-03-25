31a66ab91edf07355849aff2bbfc168d36b6cdfe
Eddie <eddieliu0206@homtail.com>
2026-03-25 10:40:16 +0800

    upgrade personal page buttons

--- personal-page/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
- const button = document.getElementById("helloButton");
-
-  
- button.onclick = function () {
- alert("Yo Eddie, you just made your first interactive page.");
-     
- };
-};
